29c3cadf868ec10ec2377545ab10404aebdb2f1c
ralexrivero <ralexrivero@gmail.com>
2022-01-17 03:53:04 -0300

    .class

--- 00-numbers.rb
+++ 00-numbers.rb
@@ -13,3 +13,18 @@ puts 2 ** 2.0
 puts 2.0 ** 3
 puts 2.0 ** 4.0
 puts
+
+a = 1
+b = 1.0
+c = '1'
+d = "Hello World"
+e = [1, 2, 3]
+f = { :a => 1, :b => 2 }
+g = { a: 1, b: 2 }
+h = (1..10)
+i = (1...10)
+j = (1..10).to_a
+
+puts h, i, j
+puts
+puts a.class, b.class, c.class, d.class, e.class, f.class, g.class, h.class, i.class, j.class
