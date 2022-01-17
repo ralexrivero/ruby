0a9d93fc295613664eec073d7a97302931d92a86
ralexrivero <ralexrivero@gmail.com>
2022-01-17 03:42:05 -0300

    basic operands

---
+++ 00-numbers.rb
@@ -0,0 +1,15 @@
+# numbers
+
+puts 1 + 3
+puts 5 - 4
+puts 2 * 3
+puts 10 / 2
+puts 10 / 3
+puts 10.0 / 3
+puts 10.0 / 2.0
+puts 10 % 3
+puts 2 ** 3
+puts 2 ** 2.0
+puts 2.0 ** 3
+puts 2.0 ** 4.0
+puts
