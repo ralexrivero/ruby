20b943623195a85819f43e7757a8e50e726eee87
ralexrivero <ralexrivero@gmail.com>
2022-01-17 03:28:56 -0300

    difference between single quotes and double quotes

--- 00-variables.rb
+++ 00-variables.rb
@@ -10,3 +10,9 @@ puts
 # print function does't print a new line
 # interpolation
 puts "Hello #{name}!, you are #{age} years old and you are #{height} m tall.\nYou live in #{city}."
+
+# string literals, double quotes and single quotes are different
+puts
+puts ('Hello ' + name + ', you are ' + age.to_s + ' years old and you are ' + height.to_s + ' m tall.\nYou live in ' + city + '.')
+puts
+puts 'Hello #{name}!, you are #{age} years old and you are #{height} m tall.\nYou live in #{city}.'
