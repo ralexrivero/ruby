3929ac1fda7dbcf4154bcd15b806f1a63b5ae927
ralexrivero <ralexrivero@gmail.com>
2022-01-17 03:22:10 -0300

    interpolation with puts

--- 00-variables.rb
+++ 00-variables.rb
@@ -6,3 +6,7 @@ height = 1.75
 city = "New York"
 
 puts age, name, height, city
+puts
+# print function does't print a new line
+# interpolation
+puts "Hello #{name}!, you are #{age} years old and you are #{height} m tall.\nYou live in #{city}."
